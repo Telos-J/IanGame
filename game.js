daa38c6d8e46ee693550c737045b8062fcb421d6
ian-pc <67145217+ian-pc@users.noreply.github.com>
2020-08-23 10:11:13 +0900

    Update game.js

--- game.js
+++ game.js
@@ -287,15 +287,15 @@ window.addEventListener("load", function () {
   };
 
   const world = new World("img/tilemaps/beach.png", 112, 112, 94, 94, 5, [
-    ["AA", "CO", "CA", "AN", "AH", "DN", "DN", "DN", "DN", "DN", "DN"],
-    ["AB", "BB", "CB", "AN", "AI", "EN", "EN", "AJ", "FF", "FF", "BJ"],
-    ["AC", "BC", "CC", "CN", "AH", "FN", "FN", "BI", "AO", "BO", "CI"],
-    ["AD", "HN", "CD", "AN", "AI", "GN", "GN", "DI", "AP", "BP", "CI"],
-    ["AN", "AN", "AN", "AN", "AH", "DN", "DN", "AK", "FG", "GG", "BK"],
-    ["AN", "BN", "AN", "AN", "AM", "AF", "BF", "CF", "DF", "AF", "BF"],
-    ["CN", "AN", "AN", "CN", "AN", "AN", "AN", "AN", "AN", "AN", "AN"],
-    ["AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "BN", "AN", "AN"],
-    ["AN", "AN", "AN", "BN", "AN", "AN", "CN", "AN", "AN", "AN", "AN"],
+    ["AA", "CO", "CA", "AN", "AH", "DN", "DN", "DN", "DN", "DN", "DN", "DN", "DN", "DN", "DN", "BI", "AN", "AN"],
+    ["AB", "BB", "CB", "AN", "AI", "EN", "EN", "AJ", "FF", "FF", "BJ", "EN", "EN", "EN", "EN", "BH", "AN", "AN"],
+    ["AC", "BC", "CC", "CN", "AH", "FN", "FN", "BI", "AO", "BO", "CI", "FN", "FN", "FN", "FN", "BI", "AN", "AN"],
+    ["AD", "HN", "CD", "AN", "AI", "GN", "GN", "DI", "AP", "BP", "CI", "GN", "GN", "GN", "GN", "BH", "AN", "AN"],
+    ["AN", "AN", "AN", "AN", "AH", "DN", "DN", "AK", "FG", "GG", "BK", "DN", "DN", "DN", "DN", "BI", "AN", "AN"],
+    ["AN", "BN", "AN", "AN", "AM", "AF", "BF", "CF", "DF", "AF", "BF", "CF", "DF", "AF", "BF", "BH", "AN", "AN"],
+    ["CN", "AN", "AN", "CN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "BM", "AN", "AN"],
+    ["AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "BN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN"],
+    ["AN", "AN", "AN", "BN", "AN", "AN", "CN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN", "AN"],
   ]);
 
   const MovingObject = function (
